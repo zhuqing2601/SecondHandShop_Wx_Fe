8d4a19d45bcaa4fa690bbe2e3b8e5975fb90e869
suwj@uerp.net <suwj@uerp.net>
2021-02-19 23:27:08 +0800

    2021/02/19 dev 添加点击商品后，展示评论的组件 css 和html还需要调试

--- pages/item/item.js
+++ pages/item/item.js
@@ -9,8 +9,29 @@ Page({
       '//img14.360buyimg.com/mobilecms/s372x372_jfs/t1/162791/1/2425/280348/5ffeb576E42da7df8/09247ac9e7737ac6.jpg!q70.dpg.webp',
       '//ci.xiaohongshu.com/1262324e-e6e4-3838-a4fa-048233c13e29?imageView2/2/w/540/format/jpg',
       '//gw.alicdn.com/bao/uploaded/i1/2166281699/O1CN012qlRbK1OQBXqOP8WQ_!!0-item_pic.jpg_500x500q90.jpg_.webp'
-    ]
+    ],
+    hidden:true,
+    nocancel:false
   },
+  cancel: function(){
+    this.setData({
+      hidden: true
+    });
+  },
+  confirm: function(){
+    this.setData({
+      nocancel: !this.data.nocancel
+    });
+    console.log("clicked confirm");
+  },
+
+  write_comment: function () {
+    this.setData({
+      hidden: false
+    })
+  },
+
+
 
   /**
    * 生命周期函数--监听页面加载
